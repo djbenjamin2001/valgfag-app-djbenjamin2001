36f3072dd464f8a2bc6bc5165c153e364ea87d49
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-08-28 11:14:39 +0200

    created search and home js

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import axios from "axios";
+
+
+const Home = () => {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Holbæk&appid={Dobebaweather}`
+  function getWeatherData () {}   
+  return ( 
+        <>
+        <h1>This is home</h1>
+        </>
+     );
+}
+ 
+export default Home;
